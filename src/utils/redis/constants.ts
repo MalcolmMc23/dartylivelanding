aabe67949e362786a08a25403cf420707d084e15
Malcolm McDonald <malcolmemcdonald@gmail.com>
2025-05-17 15:48:58 -0700

    makign it moduler

---
+++ src/utils/redis/constants.ts
@@ -0,0 +1,8 @@
+// Redis queue and key names
+export const WAITING_QUEUE = 'matching:waiting';
+export const IN_CALL_QUEUE = 'matching:in_call';
+export const ACTIVE_MATCHES = 'matching:active';
+export const USED_ROOM_NAMES = 'matching:used_room_names'; // Track used room names to prevent reuse
+export const MATCH_LOCK_KEY = "match_lock";
+export const LOCK_EXPIRY = 3; // 3 seconds instead of 5
+export const LEFT_BEHIND_PREFIX = 'left_behind:'; 
---
+++ src/utils/redis/index.ts
@@ -0,0 +1,8 @@
+// Re-export everything from the redis modules
+export * from './constants';
+export * from './roomManager';
+export * from './lockManager';
+export * from './queueManager';
+export * from './matchingService';
+export * from './disconnectionHandler';
+export * from './cleanupManager'; 
